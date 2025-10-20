950bf2f3966232d4d6660f5b57c6a93e428d302f
DuncanCragg <duncan@cilux.org>
2025-10-20 13:52:23 +0100

    sync support

--- src/onl/pico/pico-support.h
+++ src/onl/pico/pico-support.h
@@ -2,8 +2,12 @@
 #define PICO_SUPPORT_H
 
 #include "pico/stdlib.h"
+#include "hardware/sync.h"
 
 #define X       __scratch_x("")
 #define ALIGNED __attribute__((aligned(4)))
+
+#define TIMING_CRITICAL_ENTER uint32_t flags = save_and_disable_interrupts();
+#define TIMING_CRITICAL_END   restore_interrupts_from_disabled(flags);
 
 #endif
