730c3f07e3c07cfc62db9d70ed47bc0894c451d2
DuncanCragg <duncan@cilux.org>
2025-10-26 17:26:27 +0000

    config bits

--- include/onx/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#include <onx/items.h>
-
-properties* get_config(int argc, char *argv[], char* name, char* flags);
-
-#endif
--- src/onl/pico/startup.c
+++ src/onl/pico/startup.c
@@ -59,7 +59,7 @@ void __not_in_flash_func(core0_main)() {
 
   properties* config = properties_new(32);
   properties_set(config, "flags", list_vals_new_from_fixed("log-to-std log-to-led"));
-//properties_set(config, "channels", list_vals_new_from_fixed("radio"));
+  properties_set(config, "channels", list_vals_new_from_fixed("radio"));
 //properties_set(config, "test-uid-prefix", value_new("tests")); // REVISIT setting that
 
   set_up_clocks();
