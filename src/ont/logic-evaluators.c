084b2032188a5fc5db6228aeca1e4adc60096065
DuncanCragg <duncan@cilux.org>
2025-10-29 15:47:47 +0000

    only set colour while button down (so capture when not)

--- src/ont/logic-evaluators.c
+++ src/ont/logic-evaluators.c
@@ -124,12 +124,22 @@ bool evaluate_light_logic(object* o, void* d){
 }
 
 bool evaluate_bcs_logic(object* bcs, void* d){
-  uint8_t brightness = (uint8_t)255;
-  polar_t p=cartesian_to_polar((int16_t)object_property_int32(bcs, "gamepad:joystick-x"),
-                               (int16_t)object_property_int32(bcs, "gamepad:joystick-y"));
-  object_property_set_fmt(bcs, "brightness", "%d", brightness);
-  object_property_set_fmt(bcs, "colour",     "%d", p.angle);
-  object_property_set_fmt(bcs, "softness",   "%d", 255-p.radius);
+
+  bool a=object_property_is(bcs, "gamepad:a", "down");
+  bool b=object_property_is(bcs, "gamepad:b", "down");
+  bool x=object_property_is(bcs, "gamepad:x", "down");
+  bool y=object_property_is(bcs, "gamepad:y", "down");
+  bool t=object_property_is(bcs, "gamepad:start",  "down");
+  bool e=object_property_is(bcs, "gamepad:select", "down");
+
+  if(a){
+    uint8_t brightness = (uint8_t)255;
+    polar_t p=cartesian_to_polar((int16_t)object_property_int32(bcs, "gamepad:joystick-x"),
+                                 (int16_t)object_property_int32(bcs, "gamepad:joystick-y"));
+    object_property_set_fmt(bcs, "brightness", "%d", brightness);
+    object_property_set_fmt(bcs, "colour",     "%d", p.angle);
+    object_property_set_fmt(bcs, "softness",   "%d", 255-p.radius);
+  }
 
   return true;
 }
