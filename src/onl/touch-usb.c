d776932cf5e3b407d981310530e1a9c4080a5ab4
DuncanCragg <duncan@cilux.org>
2025-11-07 10:49:38 +0000

    only send touch event if changed

--- src/onl/touch-usb.c
+++ src/onl/touch-usb.c
@@ -28,10 +28,16 @@ void touch_usb_event(uint8_t* buf, uint16_t len){
       if(buf[2]!=0) break;
       uint16_t xl=buf[3]; uint16_t xh=buf[4];
       uint16_t yl=buf[5]; uint16_t yh=buf[6];
+      bool     touched=io.touched;
+      uint16_t touch_x=io.touch_x;
+      uint16_t touch_y=io.touch_y;
       io.touched=buf[1];
       io.touch_x=(xh << 8) | xl;
       io.touch_y=(yh << 8) | yl;
-      if(io_state_changed_cb) io_state_changed_cb();
+      bool changed = ( touched != io.touched ||
+                       touch_x != io.touch_x ||
+                       touch_y != io.touch_y    );
+      if(changed && io_state_changed_cb) io_state_changed_cb();
       break;
     }
   }
