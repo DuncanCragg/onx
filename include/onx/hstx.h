f38d71a574d0b33cf64754fe123cb99fdbb63520
DuncanCragg <duncan@cilux.org>
2025-11-15 22:59:41 +0000

    .

--- include/onx/hstx.h
+++ include/onx/hstx.h
@@ -9,7 +9,6 @@
 #define HSTX_VREG_V         VREG_VOLTAGE_1_30
 #define HSTX_CLOCKSPEED_KHZ (385*1000)
 #define HSTX_HSTXDIVISOR    1
-#define H_PLUS_V_PLUS
 // 385/1=385 /5=77MHz pxclk *1280x800 = 60Hz
 //    371.25 /5=74.25MHz
 #endif
@@ -18,7 +17,6 @@
 #define HSTX_VREG_V         VREG_VOLTAGE_1_30
 #define HSTX_CLOCKSPEED_KHZ (385*1000)
 #define HSTX_HSTXDIVISOR    4
-#define H_MINUS_V_MINUS
 // 385/4=96.25 /5=19.25MHz pxclk *800x480 = 39.4Hz
 #endif
 
@@ -26,18 +24,18 @@
 #define HSTX_VREG_V         VREG_VOLTAGE_1_30
 #define HSTX_CLOCKSPEED_KHZ (300*1000)
 #define HSTX_HSTXDIVISOR    2
-#define H_MINUS_V_PLUS
 // 300/2=150 /5=30MHz pxclk *800x480 = 60Hz
 #endif
 
 #ifdef WS10
-#define MODE_H_SYNC_POLARITY    1
+
+#define H_PLUS_V_PLUS
+
 #define MODE_H_FRONT_PORCH    160
 #define MODE_H_SYNC_WIDTH      32
 #define MODE_H_BACK_PORCH      40
 #define MODE_H_ACTIVE_PIXELS 1280
 
-#define MODE_V_SYNC_POLARITY    1
 #define MODE_V_FRONT_PORCH     20
 #define MODE_V_SYNC_WIDTH       6
 #define MODE_V_BACK_PORCH      30
@@ -45,13 +43,14 @@
 #endif
 
 #ifdef WS5
-#define MODE_H_SYNC_POLARITY    0
+
+#define H_MINUS_V_MINUS
+
 #define MODE_H_FRONT_PORCH     40
 #define MODE_H_SYNC_WIDTH      48
 #define MODE_H_BACK_PORCH      40
 #define MODE_H_ACTIVE_PIXELS  800
 
-#define MODE_V_SYNC_POLARITY    0
 #define MODE_V_FRONT_PORCH     13
 #define MODE_V_SYNC_WIDTH       3
 #define MODE_V_BACK_PORCH      29
@@ -59,13 +58,14 @@
 #endif
 
 #ifdef ELECROW5
-#define MODE_H_SYNC_POLARITY    0
+
+#define H_MINUS_V_PLUS
+
 #define MODE_H_FRONT_PORCH     16
 #define MODE_H_SYNC_WIDTH      80
 #define MODE_H_BACK_PORCH      96
 #define MODE_H_ACTIVE_PIXELS  800
 
-#define MODE_V_SYNC_POLARITY    1
 #define MODE_V_FRONT_PORCH      1
 #define MODE_V_SYNC_WIDTH       3
 #define MODE_V_BACK_PORCH      13
@@ -73,13 +73,14 @@
 #endif
 
 #ifdef TOSH
-#define MODE_H_SYNC_POLARITY    1
+
+#define H_PLUS_V_PLUS
+
 #define MODE_H_FRONT_PORCH     48
 #define MODE_H_SYNC_WIDTH      32
 #define MODE_H_BACK_PORCH      80
 #define MODE_H_ACTIVE_PIXELS 1280
 
-#define MODE_V_SYNC_POLARITY    1
 #define MODE_V_FRONT_PORCH      3
 #define MODE_V_SYNC_WIDTH       6
 #define MODE_V_BACK_PORCH      14
